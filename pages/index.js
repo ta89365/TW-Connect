137713adcdde34979af6cac290de1efb1b62bd5f
ta89365 <ta89365@gmail.com>
2025-09-27 16:59:12 -0500

    chore: remove Pages Router directory; use App Router only

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "system-ui" }}>
-      <h1>TW-Connect Staging</h1>
-      <p>Deployed via Vercel from the <code>staging</code> branch.</p>
-    </main>
-  );
-}
